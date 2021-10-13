db37995ffe66c171ab06ec78c6727ba595f4dda4
wasaif89 <ahmedmansour3@hotmail.com>
2021-10-13 13:57:18 +0300

    add two function in struct

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ToDoList-Project
+
+#Program name: to do list
+
+##Overview : I have designed a program that will have all the list off to do to keep me up-to-date with my goals and aims this program facilitates my reminders throughout the whole week.
+
+##Usage: I used enum ,stract,class and functions. A simple diagram list with time to keep the user up-to-date with the goals they aim to complete by the day.
+
+##Known issues: I had some of the functions were not operating and the representative not showing at its correct place but fixed it by informing it with class. And some off to do list were not in order managed to fix it in the end.
+
+##Upcoming features: I would like to see it performing on a user interface and has more interaction with the user. Where it reminds with notification .
--- ToDoList.playground/Contents.swift
+++ ToDoList.playground/Contents.swift
@@ -35,9 +35,25 @@ struct TaskItem {
 struct List {
     var listTitle : String
     var items : [TaskItem]
-    
+    // function for add new Task
     mutating func addItem (item: TaskItem ){
         self.items.append(item)
+    }
+    //function for delet Task
+    mutating func deleteItem(itemIndex: Int ){
+        if (self.items.count > itemIndex) {
+            self.items.remove(at: itemIndex)
+        }else{
+            print("can't found item")
+        }
+    }
+    // function for (updet Or change for List Or Task)
+    mutating func changesList (changes : TaskItem, itemIndex: Int){
+        if (self.items.count > itemIndex) {
+            self.items[itemIndex] = changes
+        }else{
+            print("can't found item")
+        }
     }
 }
 
@@ -51,31 +67,23 @@ class ToDoList {
     init(wasaifList: List) {
         self.wasaifList = [wasaifList]
     }
-    
+    // function for delet All List
     func removeAll() {
         self.wasaifList.removeAll()
     }
     
-
+    // function for add new List
     func addList (added : List ){
         self.wasaifList.append(added)
         
     }
     
-    func changesList (changes : TaskItem , listIndex : Int, itemIndex: Int){
-        self.wasaifList[listIndex].items[itemIndex] = changes
-
-    }
     
-    func deletList(listIndex: Int, itemIndex: Int ){
-        self.wasaifList[listIndex].items.remove(at: itemIndex)
-        
-    }
-    //read
+    //function for (reading) Tasks
     func Tasks() {
         for item in wasaifList {
             print(item.listTitle)
-        
+            
             for task in item.items {
                 print(task.title, task.time,task.day, task.note, task.pririty, task.taskStatus)
                 
@@ -85,26 +93,28 @@ class ToDoList {
     }
 }
 
-var task1 = TaskItem(title: "HW", note: "doing now", time: 8, day: .Sat, pririty: .med, taskStatus: .pending)
+var task1 = TaskItem(title: "HW", note: "doing now", time: 8, day: .Sat, pririty: .med, taskStatus: .completed)
 var task2 = TaskItem(title: "Call", note: "Daily", time: 10, day: .Fri, pririty: .high, taskStatus: .completed)
 var task3 = TaskItem(title: "Medicine", note: "now", time: 22, day: .Mon, pririty: .high, taskStatus: .pending)
 
 var task4 = TaskItem(title: "appointment", note: "Dentist appointment", time: 9, day: .Sat, pririty: .med, taskStatus: .pending)
-var task5 = TaskItem(title: "call salim", note: "coding", time: 9, day: .Sat, pririty: .med, taskStatus: .pending)
+var task5 = TaskItem(title: "cook cake", note: "orange cake", time: 9, day: .Sat, pririty: .med, taskStatus: .pending)
 var task6 = TaskItem(title: "visit", note: "visit mum", time: 6, day: .Sun, pririty: .med, taskStatus: .completed)
-var task7 = TaskItem(title: "watch", note: "film", time: 7, day: .Fri, pririty: .med, taskStatus: .pending)
+var task7 = TaskItem(title: "watch", note: "film", time: 23, day: .Fri, pririty: .med, taskStatus: .pending)
 var task8 = TaskItem(title: "call hanan", note: "go out with me", time: 6, day: .Sun, pririty: .low, taskStatus: .completed)
-var myList2 = List(listTitle: "MyList2", items: [task7,task8])
 
 var myList = List(listTitle: "MyList", items: [task1, task2,task3,task4])
 myList.addItem(item: task5)
+var myList2 = List(listTitle: "MyList2", items: [task7,task8])
 
 var newTask = ToDoList()
 newTask.addList(added: myList)
 newTask.addList(added: myList2)
-
-newTask.changesList(changes: task6 , listIndex: 0, itemIndex: 0)
+newTask.wasaifList[0].deleteItem(itemIndex: 9999)
+newTask.wasaifList[0].changesList(changes: task6, itemIndex: 90)
 newTask.Tasks()
+
+
 
 
 
